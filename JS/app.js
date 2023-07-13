722b11cdfe2423671c495de05832a037e2719388
elareh <66442150+elareh@users.noreply.github.com>
2023-07-13 17:37:57 -0300

    3raPreEntrega

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('formContacto').addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -22,21 +21,3 @@ document.getElementById('formContacto').addEventListener('submit', function(even
 
 alert('Formulario enviado. Los datos han sido guardados en el almacenamiento local.');
 });
-=======
-const empleados = [
-  {nombre: "Juan", edad: "30", sueldo: "5000"},
-  {nombre: "Roberto", edad: "40", sueldo: "1000"},
-  {nombre: "Martin", edad: "19", sueldo: "3000"},
-  {nombre: "Jorge", edad: "25", sueldo: "10000"},
-];
-
-let nombreEmpleado = prompt("Ingrese nombre de empleado");
-let edadEmpleado = prompt ("Indique a partir de que edad quisiera listar a los empleados");
-
-const resultado1 = empleados.find((el) => el.nombre === nombreEmpleado);
-const resultado2 = empleados.filter((el) => el.edad > edadEmpleado);
-
-
-console.log(resultado1);
-console.log(resultado2);
->>>>>>> 31fddc736395d8ecace6648f45d57c2f8f35a25e
